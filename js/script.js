e436db3258f001d9e1dff0bc8b4b9e2c43e2f97e
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2017-12-25 23:55:15 +0100

    Update education/skills content and styles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+//$('body').scrollspy({ target: '#menu-nav' });
+
+var offset = 80;
+
+$('.navbar a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
