0224170a92f36258b15b69833947e627c7161973
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2017-12-26 18:54:20 +0100

    Add experience, add jumbotron image

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-});
+	$('[data-toggle="tooltip"]').tooltip();
 
-//$('body').scrollspy({ target: '#menu-nav' });
+	var offset = 80;
 
-var offset = 80;
-
-$('.navbar a').click(function(event) {
+	$('.navbar a').click(function(event) {
     event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
+	});
 });
+
+
